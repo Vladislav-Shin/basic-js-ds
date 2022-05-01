ca439bd8dacc949b8ce17ac44efba80585902428
Vlad <akame678@gmail.com>
2022-05-01 21:13:08 +0500

    first commit

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,84 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.binary = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.binary;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = (data, node) => {
+      if (!node) {
+        return new Node(data);
+      } else if (data < node.data) {
+        node.l = newNode(data, node.l);
+      } else if (data > node.data) {
+        node.r = newNode(data, node.r);
+      } 
+      return node
+    };
+    this.binary = newNode(data, this.binary);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.find(data) != null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let node = this.binary;
+    while(node) {
+      if (data < node.data) {
+        node = node.l;
+      } else if (data > node.data) {
+        node = node.r;
+      } else {
+        return node;
+      }
+    }
+    return null;
   }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  remove(data, node = this.binary) {
+    if (!this.has(data)) {return}
+    if (data < node.data) {
+      node.l = this.remove(data, node.l);
+    } else if (data > node.data) {
+      node.r = this.remove(data, node.r);
+    } else {
+      if (!node.l) {
+        return node.r;
+      } else if (!node.r) {
+        return node.l;
+      } else {
+        node.data = this.min(node.r);
+        node.r = this.remove(node.data, node.r);
+      }
+    }
+    return node;
   }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min(node = this.binary) {
+    let min = node;
+    while (min.l) {
+      min = min.l;
+    }
+    return min.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let max = this.binary;
+    while (max.r) {
+      max = max.r;
+    }
+    return max.data;
   }
 }
 
